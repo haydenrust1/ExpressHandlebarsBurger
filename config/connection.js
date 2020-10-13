1b52747f4c1e05745a9cb30d979722086c698008
Haydenrust1 <haydenrust1@gmail.com>
2020-10-13 10:07:38 -0700

    jawsDB added

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 let mysql = require('mysql');
+let connection;
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '!Chickens1994',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '!Chickens1994',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect(function (err) {
     if (err) throw err;
